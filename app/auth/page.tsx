e4990e715f3843a25084fa2868fa23fa71ebc2d2
Mikolaj <mikolaj.bubacz@gmail.com>
2024-05-08 08:38:23 +0200

    Input added

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Input from "./components/Input";
+import Input from "../components/Input";
 import Image from "next/image";
 export default function Page() {
   return (
@@ -10,7 +10,37 @@ export default function Page() {
             Welcome back!
           </div>
           <div className="mx-auto">
-            <Input></Input>
+            <label className="block ml-1 text-xs font-bold text-gray-600 ">
+              EMAIL OR LOGIN
+            </label>
+            <input
+              type="text"
+              id="login"
+              className=" rounded-md border-2 py-2 px-1.5 w-96 mb-12"
+            />
+            <label className="block ml-1 text-xs font-bold text-gray-600 ">
+              PASSWORD
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="rounded-md border-2 py-2 px-1.5 w-96  "
+            />
+            <div className="ml-1 mt-1 text-xs font-bold text-blue-600">
+              Forgot your password?
+            </div>
+
+            <button className="mx-auto mt-24 w-96 py-1.5 px-2 rounded-md font-bold text-white bg-blue-500">
+              Log in
+            </button>
+            <div className="flex">
+              <div className="ml-1 mt-1 text-xs font-bold text-black">
+                Need an account?
+              </div>
+              <div className=" ml-1 mt-1 text-xs font-bold text-blue-600 ">
+                Sign up
+              </div>
+            </div>
           </div>
         </div>
         <div className="p-6 ">
---
+++ app/components/Input.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type: "text" | "password" | "email" | "number" | "tel" | "url";
+}
+
+export default function Input({ label, id, type }: InputProps) {
+  return (
+    <>
+      <label className="block ml-1 text-xs font-bold text-gray-600 ">
+        {label}
+      </label>
+      <input
+        type={type}
+        id={id}
+        className=" rounded-md border-2 py-2 px-1.5 w-96 mb-12"
+      />
+    </>
+  );
+}
