76e8ceabdcae403ecec608eda8d4351cd8a1dedb
wic3p <wiktoria.pilecka@wp.pl>
2024-05-09 12:17:59 +0200

    register

--- components/Input.tsx
+++ components/Input.tsx
@@ -9,13 +9,13 @@ interface InputProps {
 export default function Input({ label, id, type }: InputProps) {
   return (
     <>
-      <label className="block ml-1 text-xs font-bold text-gray-600 mt-8">
+      <label className="block ml-1 text-xs font-bold text-gray-600 mt-4">
         {label}
       </label>
       <input
         type={type}
         id={id}
-        className=" rounded-md border-2 py-2 px-1.5 w-96 "
+        className=" rounded-md border-2 border-black py-2 px-1.5 w-96 "
       />
     </>
   );
