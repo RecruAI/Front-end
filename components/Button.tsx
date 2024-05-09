92bbf04c0f173419dd448c051a9d5836ce10bfdf
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-05-09 23:50:37 +0200

    Import components with "@" Format code

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,12 @@
-import React from "react";
-
 interface ButtonProps {
-  title: string;
-  id: string;
+	title: string;
+	id: string;
 }
 
 export default function Button({ title, id }: ButtonProps) {
-  return (
-    <>
-      <button className="mx-auto mt-24 w-96 py-1.5 px-2 rounded-md font-bold text-white bg-blue-500">
-        {title}
-      </button>
-    </>
-  );
+	return (
+		<>
+			<button className="mx-auto mt-24 w-96 py-1.5 px-2 rounded-md font-bold text-white bg-blue-500">{title}</button>
+		</>
+	);
 }
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,22 +1,14 @@
-import React from "react";
-
 interface InputProps {
-  label: string;
-  id: string;
-  type: "text" | "password" | "email" | "number" | "tel" | "url";
+	label: string;
+	id: string;
+	type: "text" | "password" | "email" | "number" | "tel" | "url";
 }
 
 export default function Input({ label, id, type }: InputProps) {
-  return (
-    <>
-      <label className="block ml-1 text-xs font-bold text-gray-600 mt-4">
-        {label}
-      </label>
-      <input
-        type={type}
-        id={id}
-        className=" rounded-md border-2 border-black py-2 px-1.5 w-96 "
-      />
-    </>
-  );
+	return (
+		<>
+			<label className="block ml-1 text-xs font-bold text-gray-600 mt-4">{label}</label>
+			<input type={type} id={id} className=" rounded-md border-2 border-black py-2 px-1.5 w-96 " />
+		</>
+	);
 }
